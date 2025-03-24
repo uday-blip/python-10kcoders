65bea729a2ba1aa63595a91513c162ac1e994929
uday doddavarapu <udayudayharry@gmail.com>
2025-03-25 04:32:23 +0530

    Update stringmethods.py

--- stringmethods.py
+++ stringmethods.py
@@ -3,11 +3,17 @@ sentence=sentence.lower()
 vowels="aeiou"
 consonants="BCDFGHJKLMNPQRSTVWXYZ"
 consonants=consonants.lower()
+vowel_count=0
+consonant_count=0
 for i in sentence:
     if i in vowels:
         print("the vowels in sentence are:",i)
+        vowel_count+=1
+        print("the no of vowels are:,{vowel_count})
 for k in sentence:
     if k in consonants:
         print("the consonants in sentence are:",k)
+        consonant_count+=1
+        print("the no of consonants are:",{consonant_count})
 else:
     print("not found:")
